3c44182720172a5978cfbcee7535f9acee6af4f6
huangli <624513464@qq.comgitgit config --global user.email 624513464@qq.com>
2016-07-11 23:10:50 +0800

    one

--- 作品集中营/京东页面/jingdong.js
+++ 作品集中营/京东页面/jingdong.js
@@ -1,11 +1,26 @@
 /**
  * Created by Administrator on 2016/5/23.
  */
+
+$("ul.heyo").on('click','li', function(){
+    $(this).addClass('selected').siblings('.selected').removeClass('selected');
+    $(".beijing>span").text($(this).text());
+});
+
 $(function(){
     $(".Carousel").slidesjs({
         width:730,
         height:454,
-        navigation: false,
+        navigation: {
+            active: true,
+            // [boolean] Generates next and previous buttons.
+            // You can set to false and use your own buttons.
+            // User defined buttons must have the following:
+            // previous button: class="slidesjs-previous slidesjs-navigation"
+            // next button: class="slidesjs-next slidesjs-navigation"
+            effect: "slide"
+            // [string] Can be either "slide" or "fade".
+        },
         play: {
             active: true,
             // [boolean] Generate the play and stop buttons.
@@ -24,4 +39,74 @@ $(function(){
             // [number] restart delay on inactive slideshow
         }
     });
+    $('.slidesjs-previous').text("<");
+    $('.slidesjs-next').text(">");
 });
+
+$.fn.slides = function(options){
+    var $mc = this;
+    $mc.each(function(){
+        var $mc = $(this);
+        var current = 0;
+        var $pics = $mc.children().wrapAll('<div class=lists></div>').css({
+            float: 'left'
+        });
+        var $list = $mc.children().wrapAll('<div class=viewpoint></div>');
+        $list.css({
+            width:options.width*4,
+            overflow: 'hidden',
+            position: 'relative',
+            left: 0
+        });
+        var $viewpoint = $list.parent();
+        $viewpoint.css({
+            width:options.width,
+            overflow: 'hidden'
+        });
+        var hover = false;
+    var $prev = $('<button class="prev"><</button>').appendTo($viewpoint);
+        var go = function(index){
+            if(index<0){
+                index = $pics.length/4 - 1;
+            }
+            if(index>$pics.length/4-1) {
+                index = 0;
+            }
+            var left = index * (-options.width);
+            $list.stop(true,true).animate({
+                left: left
+            },function(){
+                current = index;
+                if(!hover){
+                    autoPlay();
+                }
+            })
+        };
+        var prev = function(){
+            go(current-1);
+        };
+        $prev.on('click', function(){
+            prev();
+        });
+        var $next = $('<button class="next">></button>').appendTo($viewpoint);
+        var next = function(){
+            go(current+1);
+        };
+        $next.on('click', function(){
+            next();
+        });
+        $viewpoint.on('mouseenter', function(){
+            hover = true
+        }).on('mouseleave', function(){
+            autoPlay()
+            hover = false
+        })
+
+
+    })
+}
+$('.mc').slides({
+    width:1000,
+    height: 164,
+    auto: true
+})
